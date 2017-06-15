32459aeeaeb056f140bb331fd645eda03241452c
Matthew Tran <Matthew@Matthews-Air.attlocal.net>
2017-06-14 20:16:28 -0700

    Completed functionality and user interface

--- angular.js
+++ angular.js
@@ -5,7 +5,6 @@ angular
 
 function mainCtrl() {
 	var vm = this;
-    vm.message = 'Hello, Cohort 12! If you can see this, Angular is working!';
 
     vm.denominations = {
       oneDollar: 100,
@@ -15,7 +14,20 @@ function mainCtrl() {
       penny: 1
     };
 
-    vm.amtDue = $('#cost').val();
-    vm.amtGiven = $('#given').val();
-    
+    vm.calculate = function (cost, given) {
+    vm.amtDue = Math.floor($('#cost').val() * 100);
+    vm.amtGiven = Math.floor($('#given').val() * 100);
+    vm.change = vm.amtGiven - vm.amtDue;
+    vm.dollars = Math.floor(vm.change / vm.denominations.oneDollar);
+    vm.change -= (vm.dollars * vm.denominations.oneDollar);
+    vm.quarters = Math.floor(vm.change / vm.denominations.quarter);
+    vm.change -= (vm.quarters * vm.denominations.quarter);
+    vm.dimes = Math.floor(vm.change / vm.denominations.dime);
+    vm.change -= (vm.dimes * vm.denominations.dime);
+    vm.nickels = Math.floor(vm.change / vm.denominations.nickel);
+    vm.change -= (vm.nickels * vm.denominations.nickel);
+    vm.pennies = Math.floor(vm.change / vm.denominations.penny);
+    vm.change -= (vm.pennies * vm.denominations.penny);
+    }
+
 }
--- index.js
+++
@@ -1,6 +0,0 @@
-$(document).on('ready', function() {
-    $('h1').on('click', function () {
-        alert('At least this works!');
-    })
-});
-
